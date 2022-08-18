fc656e498108bf74a9b42b396f8fa1c90b0d5cf3
Emon526 <emonats526@gmail.com>
2022-08-19 02:14:57 +0600

    Image Picker example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flutter Image Picker Example for Mobile and Web
+
+A new Flutter example project.<br> In this Project, we implemented responsive image picker for both Mobile and Web.<br> Users can pick images using mobile and web and delete the selected image if they want.
+
+## Packages used : 
+- image_picker: [link](https://pub.dev/packages/image_picker)
+- dotted_border: [link](https://pub.dev/packages/dotted_border)
---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'dart:developer';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Image Picker Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//for mobile
+File? _pickedImage;
+//for web
+Uint8List webImage = Uint8List(8);
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: size.width > 650 ? 350 : size.width * 0.45,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Theme.of(context).scaffoldBackgroundColor,
+            ),
+            child: _pickedImage == null
+                ? dottedBorder(color: Colors.blue)
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: kIsWeb
+                        ? Image.memory(
+                            webImage,
+                            fit: BoxFit.fill,
+                          )
+                        : Image.file(
+                            _pickedImage!,
+                            fit: BoxFit.fill,
+                          ),
+                  ),
+          ),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  _pickedImage = null;
+                  webImage = Uint8List(8);
+                });
+              },
+              child: const Text(
+                'Clear image',
+              )),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _pickImage() async {
+    //for mobile
+    if (!kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var selected = File(image.path);
+        setState(() {
+          _pickedImage = selected;
+        });
+      } else {
+        log('No Image Picked');
+      }
+    }
+    // for web
+    else if (kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var f = await image.readAsBytes();
+        setState(() {
+          webImage = f;
+          _pickedImage = File('a');
+        });
+      } else {
+        log('No Image Picked');
+      }
+    } else {
+      log('Something wemt Wrong');
+    }
+  }
+
+  Widget dottedBorder({required Color color}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: DottedBorder(
+        dashPattern: const [6.7],
+        borderType: BorderType.RRect,
+        color: color,
+        radius: const Radius.circular(12),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.image_outlined,
+                color: color,
+                size: 50,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: () {
+                    _pickImage();
+                  },
+                  child: const Text(
+                    'Choose an image',
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
